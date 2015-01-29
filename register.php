41cca0abfeebb3fe9ca5a4e39b05179504fa73f2
Denis Marchin <dmarch1993@gmail.com>
2015-01-28 20:05:36 -0500

    added register page

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php include("assets/templates/header.html"); ?>
+    <section id="register" class="about section">
+        <div class="container" class="intro text-center">
+            <br>
+            <br>
+            <h2 class="title text-center">Register</h2>          
+            <form action="#" method="post" class="intro text-center">
+                <input type="text" name="name" placeholder="Name" class="inputs"><br>
+                <input type="text" name="email" placeholder="E-mail" class="inputs"><br>
+                <input type="text" name="email" placeholder="Password" class="inputs"><br>
+                <input type="text" name="email" placeholder="Re-enter Password" class="inputs"><br>
+                <input type="submit" class="btn btn-cta-primary">
+            </form>
+        </div>
+    </section>
+<?php include("assets/templates/footer.html"); ?>
