043770896fa65dde410e756c6ddb4618e0e0ddeb
Denis Marchin <dmarch1993@gmail.com>
2015-01-28 20:17:25 -0500

    added templates

--- index.php
+++ index.php
@@ -1,65 +1,4 @@
-<!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
-<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
-<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
-<head>
-    <title>CommunityFund</title>
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">    
-    <link rel="shortcut icon" href="favicon.ico">  
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> 
-    <!-- Global CSS -->
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Plugins CSS -->    
-    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/plugins/prism/prism.css">
-    <!-- Theme CSS -->  
-    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head> 
-
-<body data-spy="scroll">
-    
-    <!-- ******HEADER****** --> 
-    <header id="header" class="header">  
-        <div class="container">            
-            <h1 class="logo pull-left">
-                <a class="scrollto" href="#promo">
-                    <span class="logo-title">CommunityFund</span>
-                </a>
-            </h1><!--//logo-->              
-            <nav id="main-nav" class="main-nav navbar-right" role="navigation">
-                <div class="navbar-header">
-                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button><!--//nav-toggle-->
-                </div><!--//navbar-header-->            
-                <div class="navbar-collapse collapse" id="navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active nav-item sr-only"><a class="scrollto" href="#promo">Home</a></li>
-                        <li class="nav-item"><a class="scrollto" href="#">Sign In</a></li>
-                        <li class="nav-item"><a href="/register.php">Register</a></li>
-                        <li class="nav-item"><a class="scrollto" href="#about">About</a></li>
-                        <li class="nav-item"><a href="#">Projects</a></li>                
-                        <li class="nav-item last"><a class="scrollto" href="#contact">Contact</a></li>
-                    </ul><!--//nav-->
-                </div><!--//navabr-collapse-->
-            </nav><!--//main-nav-->
-        </div>
-    </header><!--//header-->
-    
+<?php include("assets/templates/header.html"); ?>
     <!-- ******PROMO****** -->
     <section id="promo" class="promo section offset-header">
         <div class="container text-center">
@@ -157,34 +96,5 @@ the project helping it reaching a specific goal set by the initiators.</p>
         </div><!--//container-->
     </section><!--//about-->
    
-    <!-- ******CONTACT****** --> 
-    <section id="contact" class="contact section has-pattern">
-        <div class="container">
-            <div class="contact-inner">
-                <h2 class="title  text-center">Contact Us</h2>
-                <p class="intro  text-center"> Feel free to contact us if you have any questions or suggestions.</p>
-                <p class="intro text-center"> EMAIL FORM HERE YO</p>
-                <div class="clearfix"></div>
-               
-            </div><!--//contact-inner-->
-        </div><!--//container-->
-    </section><!--//contact-->  
-      
-    <!-- ******FOOTER****** --> 
-    <footer class="footer">
-        <div class="container text-center">
-            <p>Designed by <a href="#">OG Web Dev</a> </p>
-        </div><!--//container-->
-    </footer><!--//footer-->
-     
-    <!-- Javascript -->          
-    <script type="text/javascript" src="assets/plugins/jquery-1.11.1.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>    
-    <script type="text/javascript" src="assets/plugins/jquery.easing.1.3.js"></script>   
-    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>     
-    <script type="text/javascript" src="assets/plugins/jquery-scrollTo/jquery.scrollTo.min.js"></script> 
-    <script type="text/javascript" src="assets/plugins/prism/prism.js"></script>    
-    <script type="text/javascript" src="assets/js/main.js"></script>       
-</body>
-</html> 
+<?php include("assets/templates/footer.html"); ?>
 
---
+++ login.php
@@ -0,0 +1,14 @@
+<?php include("assets/templates/header.html"); ?>
+    <section id="register" class="about section">
+        <div class="container" class="intro text-center">
+            <br>
+            <br>
+            <h2 class="title text-center">Login</h2>          
+            <form action="#" method="post" class="intro text-center">
+                <input type="text" name="email" placeholder="E-mail" class="inputs"><br>
+                <input type="text" name="email" placeholder="Password" class="inputs"><br>
+                <input type="submit" class="btn btn-cta-primary">
+            </form>
+        </div>
+    </section>
+<?php include("assets/templates/footer.html"); ?>
