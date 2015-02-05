cdd152a2153845217931ea4a51f1b87a7e9ea8d0
Denis Marchin <dmarch1993@gmail.com>
2015-02-05 18:02:53 -0500

    added stuff

--- create.php
+++ create.php
@@ -3,6 +3,26 @@
     if ($_SESSION['login'] != "1"){
         header ("Location: login.php");
     }
+    //if ($_SERVER['REQUEST_METHOD'] == 'POST') {}
 ?>
 <?php include("assets/templates/header.php"); ?>
+	<section class="about section">
+		<div class="container" class="intro text-center">
+	        <br>
+	        <br>
+	        <h2 class="title text-center">I want to start a  
+	        	<select class="select-style">
+	        	<option>Art</option>
+	        	<option>Technology</option>
+	        	<option>Music</option>
+	        	<option>Photography</option>
+	        	<option>Food</option>
+	        	<option>Film and Video</option>
+	        	<option>Design</option>
+	        	<option>Games</option>
+	        	</select> project.
+	        </h2>
+	        
+	    </div>
+    </section>
 <?php include("assets/templates/footer.html"); ?>
