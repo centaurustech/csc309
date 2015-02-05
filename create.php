1cb17d162561d5142d3e8bee56b2c7f78abb9f00
Denis Marchin <dmarch1993@gmail.com>
2015-02-05 18:17:20 -0500

    stuff

--- create.php
+++ create.php
@@ -6,8 +6,11 @@
     //if ($_SERVER['REQUEST_METHOD'] == 'POST') {}
 ?>
 <?php include("assets/templates/header.php"); ?>
-	<section class="about section">
-		<div class="container" class="intro text-center">
+<style>
+  .about {background-image: url("/assets/images/clouds.jpg");}
+</style>
+	<section class="about section" >
+		<div class="container">
 	        <br>
 	        <br>
 	        <h2 class="title text-center">I want to start a  
