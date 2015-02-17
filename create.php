520ff050e89ee6e7f1861f86e749d68fd7c4e410
Denis Marchin <dmarch1993@gmail.com>
2015-02-17 16:47:29 -0500

    added some stuff to create.php

--- create.php
+++ create.php
@@ -10,19 +10,25 @@
 		<div class="container">
 	        <br>
 	        <br>
-	        <h2 class="title text-center">I want to start a  
-	        	<select class="select-style">
-	        	<option>Art</option>
-	        	<option>Technology</option>
-	        	<option>Music</option>
-	        	<option>Photography</option>
-	        	<option>Food</option>
-	        	<option>Film and Video</option>
-	        	<option>Design</option>
-	        	<option>Games</option>
-	        	</select> project.
-	        </h2>
-	        
+	        <h2 class="title text-center"> 
+		        <form action="create.php" method="post" class="intro text-center">
+		        	I want to start a <select class="select-style">
+			        	<option>Art</option>
+			        	<option>Technology</option>
+			        	<option>Music</option>
+			        	<option>Photography</option>
+			        	<option>Food</option>
+			        	<option>Film and Video</option>
+			        	<option>Design</option>
+			        	<option>Games</option>
+		        	</select> project.   
+		        	<br>
+		        	<br>
+	                <input type="title" name="title" placeholder="Project Title"><br>
+	                <input type="submit" class="btn btn-cta-primary">
+	                <br>
+	            </form>
+            </h2>
 	    </div>
     </section>
 <?php include("assets/templates/footer.html"); ?>
