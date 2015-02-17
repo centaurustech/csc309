ae19450b0c297db6df2678629c8091f2ad0b8045
Denis Marchin <dmarch1993@gmail.com>
2015-02-17 17:10:05 -0500

    added template for project info page
    access by http://localhost:8888/projectinfo.php

---
+++ projectinfo.php
@@ -0,0 +1,7 @@
+<?php include("assets/templates/header.php"); ?>
+	<section class="about section">
+		<div class="container">
+			<h2 class="title text-center"> content here </h2>
+		</div>
+	</section>
+<?php include("assets/templates/footer.html"); ?>
