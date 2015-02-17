542f05ebe52b1cced53368960363923be76775e7
Denis Marchin <dmarch1993@gmail.com>
2015-02-17 17:10:47 -0500

    dd

--- projectinfo.php
+++ projectinfo.php
@@ -1,6 +1,7 @@
 <?php include("assets/templates/header.php"); ?>
 	<section class="about section">
 		<div class="container">
+			<!-- ADD DESIGN CONTENT HERE -->
 			<h2 class="title text-center"> content here </h2>
 		</div>
 	</section>
