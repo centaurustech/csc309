55069d15de0940f306b0059435a31a9169d0e56a
Denis Marchin <dmarch1993@gmail.com>
2015-03-11 20:08:59 -0400

    funding functionality added

--- fund.php
+++ fund.php
@@ -27,32 +27,26 @@
 <br>
 <br>
 <br>
-
 <br>
-
-<!-- Vendor libraries -->
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.validation/1.13.1/jquery.validate.min.js"></script>
-
-<!-- If you're using Stripe for payments -->
-<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<input type="text" style="display:none">
+<input type="password" style="display:none">
 
 <!-- Credit card form -->
-<div class="container">
+<div class="container" style="margin-top:40px">
     <div class="row">
-        <div class="col-xs-12 col-md-4">
+    <div class="col-sm-6 col-md-4 col-md-offset-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title"><img class="pull-right" src="http://i76.imgup.net/accepted_c22e0.png">Payment Details</h3>
                 </div>
                 <div class="panel-body">
-                    <form role="form" id="payment-form">
+                    <form action="fund_success.php?id=<?=$id?>" method="post" role="form" id="payment-form">
                         <div class="row">
                             <div class="col-xs-12">
                                 <div class="form-group">
                                     <label for="cardNumber">CARD NUMBER</label>
                                     <div class="input-group">
-                                        <input type="text" class="form-control" name="cardNumber" placeholder="Valid Card Number" required autofocus data-stripe="number" />
+                                        <input type="text" class="form-control" name="cardNumber" placeholder="Valid Card Number"  autofocus data-stripe="number" autocomplete="off"/>
                                         <span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
                                     </div>
                                 </div>                            
@@ -63,31 +57,31 @@
                                 <div class="form-group">
                                     <label for="expMonth">EXPIRATION DATE</label>
                                     <div class="col-xs-6 col-lg-6 pl-ziro">
-                                        <input type="text" class="form-control" name="expMonth" placeholder="MM" required data-stripe="exp_month" />
+                                        <input type="text" class="form-control" name="expMonth" placeholder="MM"  data-stripe="exp_month" autocomplete="off" />
                                     </div>
                                     <div class="col-xs-6 col-lg-6 pl-ziro">
-                                        <input type="text" class="form-control" name="expYear" placeholder="YY" required data-stripe="exp_year" />
+                                        <input type="text" class="form-control" name="expYear" placeholder="YY"  data-stripe="exp_year" autocomplete="off"/>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xs-5 col-md-5 pull-right">
                                 <div class="form-group">
                                     <label for="cvCode">CV CODE</label>
-                                    <input type="password" class="form-control" name="cvCode" placeholder="CV" required data-stripe="cvc" />
+                                    <input type="password" class="form-control" name="cvCode" placeholder="CV"  data-stripe="cvc" autocomplete="off"/>
                                 </div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-xs-12">
                                 <div class="form-group">
-                                    <label for="couponCode">COUPON CODE</label>
-                                    <input type="text" class="form-control" name="couponCode" />
+                                    <label for="FundingAmount">Funding Amount</label>
+                                    <input type="number" class="form-control" required name="amount" autocomplete="off" />
                                 </div>
                             </div>                        
                         </div>
                         <div class="row">
                             <div class="col-xs-12">
-                                <button class="btn btn-success btn-lg btn-block" type="submit">Start Subscription</button>
+                                <button class="btn btn-success btn-lg btn-block" type="submit">Fund</button>
                             </div>
                         </div>
                         <div class="row" style="display:none;">
